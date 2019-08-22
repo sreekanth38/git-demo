1e4379cdc98336afa6503a29d39586aa44422e8c
sreekanth38 <51230190+sreekanth38@users.noreply.github.com>
2019-08-22 20:39:33 +0530

    comment added by developer 2

--- src/test/java/com/pawana/spring/security/api/SpringSecurityApplicationTests.java
+++ src/test/java/com/pawana/spring/security/api/SpringSecurityApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringSecurityApplicationTests {
-
+//this cange is done in remote repository manually
 	@Test
 	public void contextLoads() {
 	}
